b01a4591a2ffdeabee70463ae983d9a04064d84d
Jared Morrison <jrdmrrsn@gmail.com>
2024-04-16 00:52:53 -0400

    Started problem 4, am drunk tho so probably wont get far

---
+++ PE4/PE4-Driver.cpp
@@ -0,0 +1,31 @@
+/******************
+ Problem #: 4
+
+ Author: Jared Morrison
+
+ Prompt:
+    "A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit
+     numbers is 9009 = 91 * 99. Find the largest palindrome made from the product of two 3-digit numbers."
+
+ Start Date: 04/16/2024 12:48 am
+ Completion: xx/xx/xxxx xx:xx xx
+ Answer:
+ ******************/
+
+#include <iostream>
+using namespace std;
+
+int findPalindrome(int numDigits);
+
+// Three digit num max: 999 * 999 = 998001
+
+int main() {
+
+
+    return 0;
+}
+
+
+int findPalindrome(int numDigits){
+    
+}
