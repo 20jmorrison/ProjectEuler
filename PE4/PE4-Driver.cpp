ead7b5e39db8445d081d224b32a7e0ff1de50853
Jared Morrison <jrdmrrsn@gmail.com>
2024-04-17 14:51:33 -0400

    Completed PE4

--- PE4/PE4-Driver.cpp
+++ PE4/PE4-Driver.cpp
@@ -8,24 +8,61 @@
      numbers is 9009 = 91 * 99. Find the largest palindrome made from the product of two 3-digit numbers."
 
  Start Date: 04/16/2024 12:48 am
- Completion: xx/xx/xxxx xx:xx xx
- Answer:
+ Completion: 04/17/2024 02:50 pm
+ Answer: 906609
  ******************/
 
 #include <iostream>
 using namespace std;
 
-int findPalindrome(int numDigits);
+// ---- Public Functions ----
+bool isPalindrome(int numToCheck);
 
-// Three digit num max: 999 * 999 = 998001
+
+// ---- Private Functions ----
+int getNumDigits(int numToCheck);
 
 int main() {
+    int largestPalindrome = 0;
 
-
+    for(int x = 999; x > 0; x--){
+        for(int y = x; y > 0; y--){
+            int product = x * y;
+            if (isPalindrome(product)){
+                largestPalindrome = product > largestPalindrome ? product : largestPalindrome;
+            }
+        }
+    }
+    cout << "Largest palindrome from product of two 3-digit numbers: " << largestPalindrome;
     return 0;
 }
 
 
-int findPalindrome(int numDigits){
-    
+bool isPalindrome(int numToCheck){
+    int size = getNumDigits(numToCheck);
+    int tempNum = numToCheck;
+    int reversedNum = 0;
+    const int base = 10;
+
+    for (int i = size - 1; i >= 0; i--){
+        reversedNum += (tempNum % base) * pow(base, i);
+        tempNum /= base;
+    }
+
+    if (reversedNum == numToCheck){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+int getNumDigits(int numToCheck){
+    int digits = 0;
+    do{
+        numToCheck /= 10;
+        digits++;
+    }while (numToCheck > 0);
+
+    return digits;
 }
